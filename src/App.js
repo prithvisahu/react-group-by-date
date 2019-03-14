08732ac697e9a574cfc4a89c1c880647cca64e13
Prithviraj Sahu <prithvi.sahu@gmail.com>
2019-03-14 13:33:09 +0530

    group data by month

--- src/App.js
+++ src/App.js
@@ -1,39 +1,96 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import jsonData from './data.json';
 
 class App extends Component {
   state = {
-    dataKeys: [],
-    data: []
+    data: [],
+    groupBy: ''
   };
 
   componentDidMount() {
     this.setState({
-      dataKeys: [{ id: 'date', name: 'Date' }, { id: 'value', name: 'Value' }],
       data: jsonData
     });
   }
 
+  handleGroupByChange = event => {
+    const groupBy = event.target.value;
+    this.setState({ groupBy: groupBy });
+  };
+
+  groupData = () => {
+    switch (this.state.groupBy) {
+      case 'day':
+        return this.groupDataByMonth();
+      case 'week':
+        return this.groupDataByMonth();
+      case 'month':
+        return this.groupDataByMonth();
+      case 'quarter':
+        return this.groupDataByMonth();
+      case 'year':
+        return this.groupDataByMonth();
+      case '':
+      default:
+        return this.state.data;
+    }
+  };
+
+  groupDataByMonth = () => {
+    const dataWithMonthAsDate = this.state.data.map(item => {
+      const monthOfYear = moment(item.date).format('YYYY-MM');
+      return { ...item, date: monthOfYear };
+    });
+
+    const months = [...new Set(dataWithMonthAsDate.map(item => item.date))];
+
+    const dataGroupedByMonth = months.map(month => {
+      const filteredData = dataWithMonthAsDate.filter(
+        item => item.date == month
+      );
+      return {
+        date: month,
+        value: filteredData.reduce((sum, item) => sum + item.value, 0)
+      };
+    });
+
+    return dataGroupedByMonth;
+  };
+
   render() {
-    const dataKeyIds = this.state.dataKeys.map(dataKey => dataKey.id);
+    const groupedData = this.groupData();
+
     return (
       <div className="App">
+        <div>
+          <label>Group By: </label>
+          <select
+            defaultValue={this.state.groupBy}
+            onChange={this.handleGroupByChange}
+          >
+            <option value="">None</option>
+            <option value="day">Day</option>
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+            <option value="quarter">Quarter</option>
+            <option value="year">Year</option>
+          </select>
+        </div>
         <table>
           <thead>
             <tr>
               <th>#</th>
-              {this.state.dataKeys.map(dataKey => (
-                <th key={dataKey.id}>{dataKey.name}</th>
-              ))}
+              <th>Date</th>
+              <th>Value</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.data.map((item, index) => (
+            {groupedData.map((item, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
-                {dataKeyIds.map(key => (
-                  <td key={key}>{item[key]}</td>
-                ))}
+                <td>{item.date}</td>
+                <td>{item.value}</td>
               </tr>
             ))}
           </tbody>
